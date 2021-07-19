c979d0b0e93724fcc72678217bea6b3e4d0e3cdb
Amal Anwar <56563932+amal4690@users.noreply.github.com>
2021-07-19 12:45:42 -0700

    Create array.py
    not the precise answer, just a similar one.

---
+++ array.py
@@ -0,0 +1,33 @@
+#Given a binary array nums, return the maximum number of consecutive 1's in the array.
+
+def getMaxLength(arr, n):
+ 
+    # intitialize count
+    count = 0
+     
+    # initialize max
+    result = 0
+ 
+    for i in range(0, n):
+     
+        # Reset count when 0 is found
+        if (arr[i] == 0):
+            count = 0
+ 
+        # If 1 is found, increment count
+        # and update result if count
+        # becomes more.
+        else:
+             
+            # increase count
+            count+= 1
+            result = max(result, count)
+         
+    return result
+ 
+# Driver code
+arr = [1, 1, 0, 0, 1, 0, 1,
+             0, 1, 1, 1, 1]
+n = len(arr)
+ 
+print(getMaxLength(arr, n))
